bb0325d734ea756187fb61924e8d67f79179c923
vandorena <alexvd@hackclub.com>
2025-11-07 23:07:40 -0500

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import ShaderBackground from '$lib/components/ShaderBackground.svelte';
 	import NavBar from '$lib/components/NavBar.svelte';
 
@@ -8,7 +7,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/me.webp" />
 </svelte:head>
 <ShaderBackground/>
 <NavBar/>
